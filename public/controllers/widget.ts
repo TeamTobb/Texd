86fd42c86cc5424937e54dee9d7454a8c7ee9f19
borgarlie <borgar.lie@gmail.com>
2016-02-22 15:50:06 +0100

    Possibility to change widget text, but cursor is moved to start (bug)

--- public/controllers/widget.ts
+++ public/controllers/widget.ts
@@ -1,3 +1,6 @@
+// posEq convenience function from CodeMirror source
+function posEq(a, b) {return a.line == b.line && a.ch == b.ch;}
+
 export function Widget(cm) {
     // the subclass must define this.domNode before calling this constructor
     var _this = this;
@@ -8,26 +11,26 @@ export function Widget(cm) {
     var to = cm.getCursor("to");
     this.mark = cm.markText(from, to, {replacedWith: this.domNode, clearWhenEmpty: false});
 
-    if (this.enter) {
-        CodeMirror.on(this.mark, "beforeCursorEnter", function(e) {
-            // register the enter function
-            // the actual movement happens if the cursor movement was a plain navigation
-            // but not if it was a backspace or selection extension, etc.
-            var direction = posEq(_this.cm.getCursor(), _this.mark.find().from) ? 'left' : 'right';
-            cm.widgetEnter = $.proxy(_this, 'enterIfDefined', direction);
-        });
-    }
+    // if (this.enter) {
+    //     CodeMirror.on(this.mark, "beforeCursorEnter", function(e) {
+    //         // register the enter function
+    //         // the actual movement happens if the cursor movement was a plain navigation
+    //         // but not if it was a backspace or selection extension, etc.
+    //         var direction = posEq(_this.cm.getCursor(), _this.mark.find().from) ? 'left' : 'right';
+    //         cm.widgetEnter = $.proxy(_this, 'enterIfDefined', direction);
+    //     });
+    // }
     cm.setCursor(to);
     cm.refresh()
 }
 
-Widget.prototype.enterIfDefined = function(direction) {
-    if (this.mark.find()) {
-        this.enter(direction);
-    } else {
-        this.cm.refresh();
-    }
-}
+// Widget.prototype.enterIfDefined = function(direction) {
+//     if (this.mark.find()) {
+//         this.enter(direction);
+//     } else {
+//         this.cm.refresh();
+//     }
+// }
 
 Widget.prototype.range = function() {
     var find = this.mark.find()
@@ -46,35 +49,74 @@ Widget.prototype.getText = function() {
 
 export function BoldWidget(cm) {
     this.node = $(".widget-templates .bold-widget").clone();
+
+    // adding listener to change event
+    this.node[0].addEventListener("input", () => {
+        this.setValue(this.node[0].innerText);
+    }, false);
+
     this.domNode = this.node[0];
     Widget.apply(this, arguments);
-    // this.value = this.getText();
-    this.setValue(this.value);
+    this.node[0].textContent = this.value;
 }
 BoldWidget.prototype = Object.create(Widget.prototype)
-BoldWidget.prototype.enter = function(direction) {
-    // var t = this.node.find('.value');
-    var t = this.node;
-    t.focus();
-    if (direction==='left') {
-        t.setCursorPosition(0);
-    } else {
-        t.setCursorPosition(t.val().length)
-    }
-}
-
-BoldWidget.prototype.exit = function(direction) {
-    var range = this.mark.find();
-    this.cm.focus();
-    if (direction==='left') {
-        this.cm.setCursor(range.from)
-    } else {
-        this.cm.setCursor(range.to)
-    }
-}
+// BoldWidget.prototype.enter = function(direction) {
+//     // var t = this.node.find('.value');
+//     var t = this.node;
+//     t.focus();
+//     if (direction==='left') {
+//         t.setCursorPosition(0);
+//     } else {
+//         t.setCursorPosition(t.val().length)
+//     }
+// }
+//
+// BoldWidget.prototype.exit = function(direction) {
+//     var range = this.mark.find();
+//     this.cm.focus();
+//     if (direction==='left') {
+//         this.cm.setCursor(range.from)
+//     } else {
+//         this.cm.setCursor(range.to)
+//     }
+// }
 
 BoldWidget.prototype.setValue = function(val) {
     this.value = val;
-    // this.setText(this.value.toString());
-    this.node[0].textContent = this.value;
+    // var t = this.node;
+    // var pos = t.getCursorPosition();
+    // console.log(pos);
+    this.setText(" #b " + this.value.toString() + " # ");
+}
+
+// must use input to use these functions...
+// From http://stackoverflow.com/a/2897510/1200039
+$.fn.getCursorPosition = function() {
+    console.log("getting pos");
+    var input = this.get(0);
+    if (!input) return; // No (input) element found
+    if ('selectionStart' in input) {
+        // Standard-compliant browsers
+        return input.selectionStart;
+    } else if (document.selection) {
+        // IE
+        input.focus();
+        var sel = document.selection.createRange();
+        var selLen = document.selection.createRange().text.length;
+        sel.moveStart('character', -input.value.length);
+        return sel.text.length - selLen;
+    }
+}
+
+// from http://stackoverflow.com/q/499126/1200039
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+        $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+        var range = $(this).get(0).createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+    }
 }
