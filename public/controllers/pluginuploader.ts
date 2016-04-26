52df345d05ec106e3e3aae4ad07ca10300ecd121
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2016-04-26 15:00:57 +0200

    Updated plugin upload implementation to support ng2-modal

--- public/controllers/pluginuploader.ts
+++ public/controllers/pluginuploader.ts
@@ -1,6 +1,5 @@
 import {Component, Injector, provide, AfterViewInit, ViewChild, AfterContentInit} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
-// import {Modal, ModalConfig, ModalDialogInstance, YesNoModal, ICustomModal, YesNoModalContent, ICustomModalComponent} from 'angular2-modal/angular2-modal';
 import {Http, Headers, HTTP_BINDINGS, Response} from 'angular2/http';
 import {DocumentService} from '../data_access/document.ts';
 import {Alert} from 'ng2-bootstrap/ng2-bootstrap'
@@ -13,16 +12,41 @@ import {Modal} from "ng2-modal/Modal";
     directives: [Modal, Alert]
 })
 export class PluginUploader implements AfterViewInit {
-    // @ViewChild(PluginUploadModal) uploadmodal: PluginUploadModal;
     private lastResult;
     private editor;
-    public alerts = []; 
+    public alerts = [];
     public pluginname = "";
-    
+    public editorIsInitialized = false;
+
     constructor(private documentService: DocumentService) { }
-    
-    closeAlert(index){
-        this.alerts.splice(index, 1); 
+
+    onModalOpen() {
+        this.alerts = [];
+        setTimeout(() => {
+            if (!this.editorIsInitialized) {
+                this.editor = CodeMirror.fromTextArea(document.getElementById("uploadTextArea"), {
+                    height: "auto",
+                    //  viewportMargin: "Infinity",
+                    mode: "javascript",
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    smartIndent: true,
+                    autoCloseTags: true,
+                    autoIndent: true,
+                    autoCloseBrackets: true
+                })
+                this.editorIsInitialized = true;
+            }
+            var arr = [];
+            arr.push('{');
+            arr.push('  "tagname" : "",');
+            arr.push('  "html" : "",');
+            arr.push('  "attr" : [],');
+            arr.push('  "optattr" : [],');
+            arr.push('  "description" : ""');
+            arr.push('}');
+            this.editor.replaceRange(arr, { line: 0, ch: 0 }, { line: this.editor.getDoc().lastLine(), ch: 1000 });
+        }, 50); //TODO: Find an alternative to instantiate CM editor after the modal is shown
     }
     
     submit() {
@@ -32,7 +56,11 @@ export class PluginUploader implements AfterViewInit {
             pluginbody = JSON.parse(this.editor.getValue());
             if (pluginbody["tagname"] && pluginbody["html"] && pluginbody["attr"] && pluginbody["optattr"] && pluginbody["description"]) {
                 this.documentService.postPlugin({ pluginname: this.pluginname, pluginbody: pluginbody }, () => {
-                    
+                    this.alerts.push({
+                        msg: 'Success!',
+                        type: 'success',
+                        closeable: 'true'
+                    })
                 })
             } else {
                 //Valid JSON, but not the required elements for a complete pluginbody
@@ -54,29 +82,7 @@ export class PluginUploader implements AfterViewInit {
         }
     }
 
-    onModalOpen() {
-        console.log("on modal open");
-        setTimeout(() => {
-            this.editor = CodeMirror.fromTextArea(document.getElementById("uploadTextArea"), {
-                height: "auto",
-                //  viewportMargin: "Infinity",
-                mode: "javascript",
-                lineNumbers: true,
-                lineWrapping: true,
-                smartIndent: true,
-                autoCloseTags: true,
-                autoIndent: true,
-                autoCloseBrackets: true
-            })
-            var arr = [];
-            arr.push('{');
-            arr.push('  "tagname" : "",');
-            arr.push('  "html" : "",');
-            arr.push('  "attr" : [],');
-            arr.push('  "optattr" : [],');
-            arr.push('  "description" : ""');
-            arr.push('}');
-            this.editor.getDoc().replaceRange(arr, { line: 0, ch: 0 }, { line: this.editor.getDoc().lastLine(), ch: 1000 });
-        }, 100);
+    closeAlert(index) {
+        this.alerts.splice(index, 1);
     }
 }
